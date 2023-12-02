d12b7b1117bf1e08a321a485e046a349973545b6
itsferro <feras.mamon.alzaidi@gmail.com>
2023-12-01 17:17:17 -0800

    Task number 4 attempt number 2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import hidden_4
 
 if __name__ == "__main__":
     defined_names = [
-            name for name in dir(module_name) if not name.startswith("__")
+            name for name in dir(hidden_4) if not name.startswith("__")
     ]
 
     for name in defined_names:
