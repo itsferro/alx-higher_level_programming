acbf587feeb9c034119e962006fae631ffc64938
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-05 21:55:44 -0800

    Task number 101 attempt number 4

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
+
 class LockedClass:
+    """
+    This is a locked class that restricts attribute assignment to only 'first_name'.
+
+    Attributes:
+        __slots__ (list): A list specifying the allowed attributes of the class.
+
+    Methods:
+        __init__(): Initializes an instance of the LockedClass.
+    """
+
     __slots__ = ['first_name']
 
     def __init__(self):
+        """
+        Initializes an instance of the LockedClass.
+
+        Example:
+            obj = LockedClass()
+        """
         pass
