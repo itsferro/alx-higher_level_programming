10bd63c795dd14e17d644018165f6c2995f6bdfd
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-09 17:06:06 -0800

    task number 1 attempt number 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,9 @@
 writes a string to a text file (UTF8)
 and returns the number of characters written:
 """
+
+
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding="utf-8") as work_file:
+    with open(filename, 'w') as work_file:
         characters_written = work_file.write(text)
         return characters_written
