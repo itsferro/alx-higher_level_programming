7326aa4ab9eabb3e49308ef928dcd3deea3b62cd
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-09 16:50:18 -0800

    Task 0 attempt number 2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+reads a text file (UTF8) and prints it to stdout:
+"""
 def read_file(filename=""):
     with open(filename, encoding="UTF8") as work_file:
         file_content = work_file.read()
