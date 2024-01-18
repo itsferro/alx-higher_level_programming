82e8eadb82e50e27445a4f294013c7f11724325e
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-17 22:24:44 -0800

    Task number 5 attempt number 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     '''
         removes all characters c and C from a string.
     '''
-    if my_string:
+    if my_string is not None:
         new_string = ""
         for i in my_string:
             if i != 'C' and i != 'c':
