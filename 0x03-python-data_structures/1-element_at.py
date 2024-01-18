93d2e8ac066932431b333857fd3132d852f1e3d4
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-17 20:46:39 -0800

    Task number 1 attempt number 2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,6 +5,7 @@ def element_at(my_list, idx):
         takes a list and index
         returns element at index.
     '''
-    if idx < 0 or idx > len(my_list):
+    my_list_length = len(my_list)
+    if idx < 0 or idx >= my_list_length or my_list_length == 0:
         return none
     return my_list[idx]
---
+++ 0x03-python-data_structures/nothing.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+my_list = []
+
+print('my_list length is: {}'.format(len(my_list)))
