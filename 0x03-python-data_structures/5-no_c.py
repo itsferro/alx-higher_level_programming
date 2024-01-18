4451f5cf8dbc3d96521065560caf3f191f2f0528
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-17 22:21:13 -0800

    Task number 5 attempt number 1

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+
+def no_c(my_string):
+    '''
+        removes all characters c and C from a string.
+    '''
+    if my_string:
+        new_string = ""
+        for i in my_string:
+            if i != 'C' and i != 'c':
+                new_string += i
+        return str(new_string)
