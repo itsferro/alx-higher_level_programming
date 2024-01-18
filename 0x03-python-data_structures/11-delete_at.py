1315c1afaf9d125405845c20e3a6a5668876238c
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-18 01:32:56 -0800

    Task number 11 attempt number 1

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    '''
+        deletes the item at a specific position in a list.
+    '''
+    new_list = []
+    if my_list:
+        list_len = len(my_list)
+        if idx >= 0 and idx < list_len:
+            for i in range(list_len):
+                if i >= idx and i < (list_len - 1):
+                    my_list[i] = my_list[i + 1]
+                if i == idx and i == (list_len - 1):
+                    del my_list[idx]
+                if i == (list_len - 1):
+                    del my_list[i]
+
+    return my_list
