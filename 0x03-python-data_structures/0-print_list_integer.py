0b6701a32c455dbdf9e39c4684405e7cce2ebaf5
itsferro <feras.mamon.alzaidi@gmail.com>
2024-01-17 19:02:45 -0800

    Task number 0 attempt number 3

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
+    '''
+        takes a list of integers
+        print the lintegers one integer ber line.
+    '''
     for x in my_list:
         print("{}".format(x))
